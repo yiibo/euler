107532de683f0e94f5ea8fc43fee57eb0c4d1af0
Yibo Weng <yibow046@gmail.com>
2016-07-23 00:27:49 +1200

    Project Euler sum of multiples of 3 and 5

---
+++ mult3and5/sumMultiple.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+
+# Author: Yibo Weng
+# Date: 21/07/2016
+
+# Find sum of all natural numbers below 1000 that are multiples of 3 or 5
+class SumMultiple
+  def initialize(input_no,upper)
+    @i, @u = input_no, upper
+  end
+
+  def solve()
+    # Boundary Conditions
+    n = @u/@i
+    if @u % @i == 0
+      n-=1
+    end
+
+    # Arithmetic Sequence Formula
+    @sum = n * @i *(1 + n)/2
+  end
+
+  def getSumMultiple
+    solve()
+    @sum
+  end
+end
+
+upper=1000
+sm3=SumMultiple.new(3,upper)
+sm5=SumMultiple.new(5,upper)
+sm15=SumMultiple.new(15,upper)
+puts sm3.getSumMultiple() + sm5.getSumMultiple() - sm15.getSumMultiple()
