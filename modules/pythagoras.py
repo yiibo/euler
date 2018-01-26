befc1df7050342802d8015f51abca978c6c23bbd
Yibo Weng <yibow046@gmail.com>
2018-01-26 15:09:33 +0000

    Updated pythagoras

--- modules/pythagoras.py
+++ modules/pythagoras.py
@@ -1,14 +1,16 @@
 #!/usr/local/bin/python2
 import math
 
-def pythagoras(number):
+def special_pythagoras(number):
     """ Returns a set of natural pythagorean numbers
     """
-    for a in range(1,number):
-        for b in range(a,number):
+    a = 1
+    while 4 * a < number:
+        for b in range(a,number - 2 * a):
             c = math.sqrt(a**2 + b**2)
             if a + b + c == number:
                 return a,b,c
+        a+=1
 
     return 0,0,0
 
@@ -16,4 +18,4 @@ def pythagoras_product(a,b,c):
     return a * b * c
 
 if __name__ == "__main__":
-    print pythagoras_product(*pythagoras(1000))
+    print pythagoras_product(*special_pythagoras(1000))
